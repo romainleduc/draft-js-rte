1c508532d9e964bee241f79bf8e62e15ab97afea
Romain Leduc <romain.leduc.dev@gmail.com>
2021-10-08 18:52:43 +0200

    [examples] Add font size example

---
+++ examples/font-size/src/InlineStyleToggleButton.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useInline } from "draft-js-rte";
+import clsx from "clsx";
+
+const InlineStyleToggleButton = ({ className, value, ...other }) => {
+  const { selected, ...inlineProps } = useInline(value);
+
+  return (
+    <span
+      className={clsx(
+        className,
+        "RichEditor-styleButton",
+        selected && "RichEditor-activeButton"
+      )}
+      {...other}
+      {...inlineProps}
+    />
+  );
+};
+
+export default InlineStyleToggleButton;
