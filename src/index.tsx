7d12b9027a6264535607a30519aac6cf40c0afa8
Romain Leduc <romain.leduc.dev@gmail.com>
2022-01-02 18:36:18 +0100

    [clean] Lint fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 export { Editor, EditorProvider } from './components';
 export type { EditorProps, EditorProviderProps } from './components';
 
-export { useBlockType, useInlineStyle, useTextAlign, useAtomicMedia } from './hooks';
+export {
+  useBlockType,
+  useInlineStyle,
+  useTextAlign,
+  useAtomicMedia,
+} from './hooks';
 
 export {
   getDefaultBlockRenderer,
